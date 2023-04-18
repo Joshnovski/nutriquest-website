dcbc863363db2b10ed38f05ac30028c9b7a2b493
Josh Morphew <josh.morphew@hotmail.com>
2023-04-18 19:25:44 +1000

    This is my first commit

---
+++ src/components/LoginComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LoginComponent() {
+  return (
+    <div>
+        <h1>LoginComponent</h1>
+    </div>
+  )
+}
---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGi2ZZC849LFY3viA6fsjWhCUxs7WbY88",
+  authDomain: "nutriquest-website.firebaseapp.com",
+  projectId: "nutriquest-website",
+  storageBucket: "nutriquest-website.appspot.com",
+  messagingSenderId: "33505663167",
+  appId: "1:33505663167:web:59648cf6373d2db82fce0a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
