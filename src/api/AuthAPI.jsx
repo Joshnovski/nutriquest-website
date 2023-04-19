5cc41542b093bc8e291233b776fff317d4b80868
Josh Morphew <josh.morphew@hotmail.com>
2023-04-19 20:47:46 +1000

    Large Changes

--- src/api/AuthAPI.jsx
+++ src/api/AuthAPI.jsx
@@ -1,1 +1,11 @@
-export const LoginAPI = () => {};
+import { signInWithEmailAndPassword} from "firebase/auth";
+import { auth } from "../firebaseConfig";
+
+export const LoginAPI = (email, password) => {
+    try {
+       let response = signInWithEmailAndPassword(auth, email, password);
+       return response;
+    } catch(err) {
+      return err;
+    }
+};  
--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
-import { LoginAPI } from '../api/AuthAPI'
+import { LoginAPI } from '../api/AuthAPI';
 import "../Sass/LoginComponent.scss";
+
 export default function LoginComponent() {
+  const [credentials, setCredentials] = useState({});
+  const login = () => {
+    let res = LoginAPI(credentials.email, credentials.password);
+    console.log(res);
+  };
   return (
-    <div>
+    <div className="login-wrapper">
         <h1>LoginComponent</h1>
-        <button className='login-btn'>Log IN to Link</button>
+        <div className="auth-inputs">
+          <input
+            onChange={(event) =>
+              setCredentials({...credentials, email: event.target.value })
+            }
+            className="common-input"
+            placeholder="Enter your Email"
+          />
+          <input
+            onChange={(event) =>
+              setCredentials({...credentials, password: event.target.value })
+            }
+            className="common-input"
+            placeholder="Enter your Password"
+          />
+        </div>
+        <button onClick={login} className='login-btn'>
+          Log in to NutriQuest
+        </button>
     </div>
   )
 }
