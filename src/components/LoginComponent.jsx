63ce76d2850fc334d396f9ea8c3c8fb3b9727340
Josh Morphew <josh.morphew@hotmail.com>
2023-04-23 21:20:53 +1000

    Added in other sign in options and link to sign up page.

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import { LoginAPI } from "../api/AuthAPI";
 import nutriquestLogo from "../assets/nutriquestLogo.png";
 import "../Sass/LoginComponent.scss";
+import GoogleButton from "react-google-button";
 
 export default function LoginComponent() {
   const [credentials, setCredentials] = useState({});
   const login = async () => {
     try{
       let res = await LoginAPI(credentials.email, credentials.password);
-      console.log(res?.user);
+      toast.success("Signed In to Linkedin!");
     } catch(err){
       console.log(err);
     }
@@ -16,32 +17,44 @@ export default function LoginComponent() {
   return (
     <div className="login-wrapper">
         <img src={nutriquestLogo} className="nutriquestLogo" />
-
         <div className="login-wrapper-inner">
-        <h1 className="heading">Sign in</h1>
-        <p className="sub-heading">Stay on top of your health and fitness journey</p>
-        
-        <div className="auth-inputs">
-          <input
-            onChange={(event) =>
-              setCredentials({...credentials, email: event.target.value })
-            }
-            type='email'
-            className="common-input"
-            placeholder="Email or Phone"
-          />
-          <input
-            onChange={(event) =>
-              setCredentials({...credentials, password: event.target.value })
-            }
-            type='password'
-            className="common-input"
-            placeholder="Password"
-          />
+          <h1 className="heading">Sign in</h1>
+          <p className="sub-heading">Stay on top of your health and fitness journey</p>
+          
+          <div className="auth-inputs">
+            <input
+              onChange={(event) =>
+                setCredentials({...credentials, email: event.target.value })
+              }
+              type='email'
+              className="common-input"
+              placeholder="Email or Phone"
+            />
+            <input
+              onChange={(event) =>
+                setCredentials({...credentials, password: event.target.value })
+              }
+              type='password'
+              className="common-input"
+              placeholder="Password"
+            />
+          </div>
+          <button onClick={login} className='login-btn'>
+            Sign in to NutriQuest
+          </button>
         </div>
-        <button onClick={login} className='login-btn'>
-          Log in to NutriQuest
-        </button>
+        <hr class="hr-text" data-content="or"/>
+        <div className = "google-btn-container">
+          <GoogleButton
+            className="button" 
+            onClick={() => {
+              console.log("Google button clicked");
+            }}
+          />
+
+          <p className="go-to-signup">
+            New to NutriQuest? <span className="join-now">Join now</span>
+          </p>
         </div>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider, } from "react-router-dom";
 import { router } from './routes'
 import { app, auth } from './firebaseConfig';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <ToastContainer />
   </React.StrictMode>,
 )
