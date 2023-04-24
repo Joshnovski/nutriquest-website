00d3c57a456a5c8aeb3afd3d0c46398b2e5cc85e
Josh Morphew <josh.morphew@hotmail.com>
2023-04-25 00:13:21 +1000

    log off for the day

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import HomeComponent from '../components/HomeComponent';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../firebaseConfig';
 
 export default function Home() {
+    let navigate = useNavigate();
+    useEffect(() => {
+        onAuthStateChanged(auth, res => {
+            if (!res?.accessToken){
+                navigate('/')
+                }
+        });
+    }, []);
   return <HomeComponent />;
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react'
 import LoginComponent from '../components/LoginComponent';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../firebaseConfig';
+
 
 export default function Login() {
+  let navigate = useNavigate();
+  useEffect(() => {
+    onAuthStateChanged(auth, res => {
+        if (res?.accessToken){
+          navigate('/home')
+        }
+    });
+}, []);
   return <LoginComponent />;
 }
 
