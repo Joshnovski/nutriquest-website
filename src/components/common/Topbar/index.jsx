65e2d64681ace5bf7b0c28d89fb0f1200ee138f0
Josh Morphew <josh.morphew@hotmail.com>
2023-04-26 22:15:01 +1000

    Began setting up home page content

--- src/components/common/Topbar/index.jsx
+++ src/components/common/Topbar/index.jsx
@@ -6,15 +6,20 @@ import { FaUser, FaBell } from 'react-icons/fa';
 import { BsFillBriefcaseFill } from 'react-icons/bs';
 import { GoSearch } from 'react-icons/go';
 import userGrey from '../../../assets/userGrey.png';
+import { useNavigate } from 'react-router-dom';
 
 export default function Topbar() {
+  let navigate = useNavigate();
+  const goToRoute = (route) => {
+    navigate(route);
+  }
   return ( <div className='topbar-main'>
     <img className='nutriquest-Logo' src={nutriquestLogo} alt='nutriquestLogo'/>
     <div className='react-icons'> 
       {/* Wrap all icons in the same div so we can arrange them */}
       <GoSearch size = {23} className='react-icon'/>
-      <AiFillHome size = {26} className='react-icon'/>
-      <FaUser size = {23} className='react-icon'/>
+      <AiFillHome size = {26} className='react-icon' onClick={() => goToRoute('/home')}/>
+      <FaUser size = {23} className='react-icon' onClick={() => goToRoute('/profile')}/>
       <BsFillBriefcaseFill size = {26}  className='react-icon'/>
       <AiFillMessage size = {26} className='react-icon'/>
       <FaBell size = {23} className='react-icon'/>
