b01da486d6b43241d52b1706163d1b897b6b732e
Josh Morphew <josh.morphew@hotmail.com>
2023-04-26 20:58:39 +1000

    Finished tool bar

--- src/components/common/Topbar/index.jsx
+++ src/components/common/Topbar/index.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import nutriquestLogo from '../../../assets/nutriquestLogo.png';
 import './index.scss';
+import { AiFillHome, AiFillMessage } from 'react-icons/ai';
+import { FaUser, FaBell } from 'react-icons/fa';
+import { BsFillBriefcaseFill } from 'react-icons/bs';
+import { GoSearch } from 'react-icons/go';
+import userGrey from '../../../assets/userGrey.png';
 
 export default function Topbar() {
-  return (
-    <div>Topbar</div>
-  )
+  return ( <div className='topbar-main'>
+    <img className='nutriquest-Logo' src={nutriquestLogo} alt='nutriquestLogo'/>
+    <div className='react-icons'> 
+      {/* Wrap all icons in the same div so we can arrange them */}
+      <GoSearch size = {23} className='react-icon'/>
+      <AiFillHome size = {26} className='react-icon'/>
+      <FaUser size = {23} className='react-icon'/>
+      <BsFillBriefcaseFill size = {26}  className='react-icon'/>
+      <AiFillMessage size = {26} className='react-icon'/>
+      <FaBell size = {23} className='react-icon'/>
+    </div>
+    <img className='userGrey' src={userGrey} alt='userGrey'/>
+  </div>
+  );
 }
