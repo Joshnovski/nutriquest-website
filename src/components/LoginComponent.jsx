71e35a1cf481c965bbe21a46ed7979e0f70c3796
Josh Morphew <josh.morphew@hotmail.com>
2023-05-16 20:56:11 +1000

    Polished Login a bit. broke join page

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -10,12 +10,12 @@ export default function LoginComponent() {
   let navigate = useNavigate();
   const [credentials, setCredentials] = useState({});
   const login = async () => {
-    try{
+    try {
       let res = await LoginAPI(credentials.email, credentials.password);
       toast.success("Signed In to NutriQuest");
       localStorage.setItem("userEmail", res.user.email);
       navigate("/home");
-    } catch(err){
+    } catch (err) {
       console.log(err);
       toast.error("Incorrect Email or Password");
     }
@@ -27,44 +27,55 @@ export default function LoginComponent() {
   };
   return (
     <div>
-      <img src={nutriquestLogo} className="nutriquestLogo" />
-        <div className="block-positioning">
-          <div className="login-wrapper">
-            <div className="login-wrapper-inner">
-              <h1 className="heading">Sign in</h1>
-              <p className="sub-heading">Stay on top of your health and fitness journey</p>
-              <div className="auth-inputs">
-                <input
-                  onChange={(event) =>
-                    setCredentials({...credentials, email: event.target.value })
-                  }
-                  type='email'
-                  className="common-input"
-                  placeholder="Email or Phone"
-                />
-                <input
-                  onChange={(event) =>
-                    setCredentials({...credentials, password: event.target.value })
-                  }
-                  type='password'
-                  className="common-input"
-                  placeholder="Password"
-                />
-              </div>
-              <button onClick={login} className='login-btn'>
-                Sign in to NutriQuest
-              </button>
+      <div className="promotion-header">
+        <img src={nutriquestLogo} className="nutriquestLogo" />
+        <h1 className="nutriquest-name">NutriQuest</h1>
+      </div>
+      <div className="block-positioning">
+        <div className="login-wrapper">
+          <div className="login-wrapper-inner">
+            <h1 className="heading">Sign in</h1>
+            <p className="sub-heading">
+              Stay on top of your health and fitness journey
+            </p>
+            <div className="auth-inputs">
+              <input
+                onChange={(event) =>
+                  setCredentials({ ...credentials, email: event.target.value })
+                }
+                type="email"
+                className="common-input"
+                placeholder="Email or Phone"
+              />
+              <input
+                onChange={(event) =>
+                  setCredentials({
+                    ...credentials,
+                    password: event.target.value,
+                  })
+                }
+                type="password"
+                className="common-input"
+                placeholder="Password"
+              />
             </div>
-              <hr className="hr-text" data-content="or"/>
-            <div className = "google-btn-container">
-              <GoogleButton className="button" onClick={googleSignIn} />
-              
-              <p className="go-to-signup">
-                New to NutriQuest? <span className="join-now" onClick={() => navigate('/Register')}>Join now</span>
-              </p>
-            </div>
+            <button onClick={login} className="login-btn">
+              Sign in to NutriQuest
+            </button>
+          </div>
+          <hr className="hr-text" data-content="or" />
+          <div className="google-btn-container">
+            <GoogleButton className="button" onClick={googleSignIn} />
+
+            <p className="go-to-signup">
+              New to NutriQuest?{" "}
+              <span className="join-now" onClick={() => navigate("/Register")}>
+                Join now
+              </span>
+            </p>
           </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
